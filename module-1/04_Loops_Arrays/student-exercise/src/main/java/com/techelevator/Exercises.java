c50f37bab05b2c6cb200208b17ceef3cc811dbaa
Shane Craig <shanecraig99@live.com>
2020-09-17 15:13:12 -0400

    Submitting Week 1 Day 4 Exercise

--- module-1/04_Loops_Arrays/student-exercise/src/main/java/com/techelevator/Exercises.java
+++ module-1/04_Loops_Arrays/student-exercise/src/main/java/com/techelevator/Exercises.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.lang.reflect.Array;
+
 public class Exercises {
 	
 	/*
@@ -10,9 +12,31 @@ public class Exercises {
 	 firstLast6([13, 6, 1, 2, 3]) → false
 	 */
 	public boolean firstLast6(int[] nums) {
-		return false;
+		
+		boolean outputVariable;
+		
+		outputVariable = (nums[0] == 6 || nums[nums.length - 1] == 6) ? true : false;
+		
+		return outputVariable;
 	}
-
+	
+		
+		/* 		long format:
+		     if (nums [0] == 6 || nums [nums.length -1] == 6) {
+			   return true;
+		}
+			   return false;
+	}
+		*/
+	
+	/* for( int i = 0 ; i < arr.length; i++){
+	 		if (arr[i] == 6) {
+	 			return true;
+	 		}
+	  	}
+	  		return false;
+	 */
+	
 	/*
 	 2. Given an array of ints, return true if the array is length 1 or more, and the first element and
 	 the last element are equal.
@@ -21,7 +45,12 @@ public class Exercises {
 	 sameFirstLast([1, 2, 1]) → true
 	 */
 	public boolean sameFirstLast(int[] nums) {
-		return false;
+		
+		boolean arrayLength;
+		
+		arrayLength = (nums.length >= 1) && (nums[0] == nums[nums.length -1]) ? true : false;
+		
+		return arrayLength;
 	}
 
 	/*
@@ -29,7 +58,10 @@ public class Exercises {
 	 makePi() → [3, 1, 4]
 	 */
 	public int[] makePi() {
-		return new int[] {};
+		
+		int [] pi = {3,1,4};
+		
+		return pi;
 	}
 
 	/*
@@ -40,7 +72,12 @@ public class Exercises {
 	 commonEnd([1, 2, 3], [1, 3]) → true
 	 */
 	public boolean commonEnd(int[] a, int[] b) {
-		return false;
+		
+		boolean commonEndCheck;
+		
+		commonEndCheck = (a[0] == b [0] || a[a.length -1] == b[b.length -1]) ? true : false;
+		
+		return commonEndCheck;
 	}
 
 	/*
@@ -50,7 +87,14 @@ public class Exercises {
 	 sum3([7, 0, 0]) → 7
 	 */
 	public int sum3(int[] nums) {
-		return 0;
+		
+		int arraySum = 0;
+		
+		for (int i = 0; i <= nums.length -1; i++) {
+			arraySum += nums[i];
+		}
+		
+		return arraySum;
 	}
 
 	/*
@@ -61,7 +105,10 @@ public class Exercises {
 	 rotateLeft3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] rotateLeft3(int[] nums) {
-		return new int[] {};
+		
+		int [] rotated = {nums[1], nums[2], nums[0]};
+		
+		return rotated;
 	}
 
 	/*
@@ -72,8 +119,12 @@ public class Exercises {
 	 reverse3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] reverse3(int[] nums) {
-		return new int[] {};
-	}
+		
+		int [] reverse = {nums[2], nums [1], nums[0]};
+		
+		return reverse;
+		}
+	
 
 	/*
 	 8. Given an array of ints length 3, figure out which is larger between the first and last elements 
@@ -83,7 +134,19 @@ public class Exercises {
 	 maxEnd3([2, 11, 3]) → [3, 3, 3]
 	 */
 	public int[] maxEnd3(int[] nums) {
-		return new int[] {};
+		if (nums[0] >= nums[2]) {
+			nums [1] = nums [0];
+			nums [2] = nums [0];
+			return nums;
+		}
+		else if ( nums[2] > nums[0]) {
+			nums [0] = nums [2];
+			nums [1] = nums [2];
+			return nums;
+		}
+		return nums;
+		
+		
 	}
 
 	/*
@@ -94,7 +157,13 @@ public class Exercises {
 	 sum2([1, 1, 1, 1]) → 2
 	 */
 	public int sum2(int[] nums) {
-		return 0;
+		if (nums.length == 1) {
+			return nums[0];
+		}
+		else if (nums.length == 0) {
+			return 0;
+		}
+		return nums [0] + nums [1];
 	}
 
 	/*
@@ -105,7 +174,13 @@ public class Exercises {
 	 middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
 	 */
 	public int[] middleWay(int[] a, int[] b) {
-		return new int[] {};
+		int [] middleArray = new int [2];
+		
+		middleArray [0] = a [1];
+		
+		middleArray [1] = b [1];
+		
+		return middleArray;
 	}
 
 	/*
@@ -116,7 +191,16 @@ public class Exercises {
 	 countEvens([1, 3, 5]) → 0
 	 */
 	public int countEvens(int[] nums) {
-		return 0;
+		int totalEvens = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			
+			if (nums[i] % 2 == 0 ) {
+				
+				totalEvens ++;
+			}
+		}
+		return totalEvens;
 	}
 
 	/*
@@ -130,7 +214,24 @@ public class Exercises {
 	 sum13([1, 2, 2, 1, 13, 3, 4]) → 10
 	 */
 	public int sum13(int[] nums) {
-		return 0;
+		int sumSum = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			
+			if (nums [i] == 13) {
+				
+				i ++;
+				
+			}
+			else if (nums [i] != 13 ) {
+				
+				sumSum += nums[i];
+				
+			
+			
+			}
+		}
+		return sumSum;
 	}
 
 	/*
@@ -140,6 +241,18 @@ public class Exercises {
 	 has22([2, 1, 2]) → false
 	 */
 	public boolean has22(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			
+			if (nums[i] == 2 && i > 0 && nums[i - 1] == 2) {
+				
+				return true;
+			}
+			
+			if (nums[i] == 2 && i < nums.length -1 && nums[i + 1] == 2) {
+				
+				return true;
+			}
+		}
 		return false;
 	}
 	
@@ -150,7 +263,15 @@ public class Exercises {
 	 lucky13([1, 2, 4]) → false
 	 */
 	public boolean lucky13(int[] nums) {
-		return false;
+		
+		for (int i = 0; i < nums.length; i++) {
+			
+			if (nums[i] == 1 || nums [i] == 3) {
+				
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/*
@@ -160,7 +281,17 @@ public class Exercises {
 	 sum28([1, 2, 3, 4]) → false
 	 */
 	public boolean sum28(int[] nums) {
-		return false;
+		int sumTotal = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			
+			if (nums[i] == 2 ) {
+				
+				sumTotal += 2;
+			}
+			
+		}
+		return (sumTotal == 8) ? true : false;
 	}
 
 }
--- module-1/04_Loops_Arrays/student-lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/04_Loops_Arrays/student-lecture/src/main/java/com/techelevator/Lecture.java
@@ -7,7 +7,7 @@ public class Lecture {
     */
     public int[] returnArray() {
         int[] array = { 80, 8080, 443 };
-        return null;
+        return array;
     }
 
     /*
@@ -15,7 +15,7 @@ public class Lecture {
     */
     public int returnFirstElement() {
         int[] portNumbers = { 80, 8080, 443 };
-        return 1;
+        return portNumbers[0];
     }
 
     /*
@@ -23,21 +23,21 @@ public class Lecture {
     */
     public int returnLastElement() {
         int[] portNumbers = { 80, 8080, 443 };
-        return 1;
+        return portNumbers[portNumbers.length-1];
     }
 
     /*
     4. Return the first element of the array from the parameters
     */
     public int returnFirstElementOfParam(int[] passedInArray) {
-        return 1;
+        return passedInArray[0];
     }
 
     /*
     5. Return the last element of the array from the parameters
     */
     public int returnLastElementOfParam(int[] passedInArray) {
-        return 1;
+        return passedInArray[passedInArray.length -1];
     }
 
     /*
@@ -45,14 +45,15 @@ public class Lecture {
        return it? There are a couple of different ways of doing this, what can you come up with?
     */
     public int returnVariableFromBlock(int number) {
-
+    	int result = number * 5;
+    	
         { // A new block with scoped variables
-
-            int result = number * 5;
+        
+        	return result; 
 
         } // the result variable disappears here
 
-        return number; // We want to return result here. How?
+        // We want to return result here. How?
     }
 
     /*
@@ -67,7 +68,7 @@ public class Lecture {
             result *= multiplier;
         }
 
-        return result == 1; // <-- Change the number to match result and make this be true
+        return result == 50; // <-- Change the number to match result and make this be true
     }
 
     /*
@@ -88,7 +89,7 @@ public class Lecture {
             double eight = five + three;
         }
 
-        return 0;
+        return one;
     }
 
     /*
@@ -104,7 +105,7 @@ public class Lecture {
             counter++;
         }
 
-        return counter == 1; // What should the number be to return true?
+        return counter == 4; // What should the number be to return true?
     }
 
     /*
@@ -116,7 +117,7 @@ public class Lecture {
         int counter = 0;
 
         //     Start;       Keep going while         Increment by one;
-        for (int i = 1; i < arrayToLoopThrough.length; i++) {
+        for (int i = 0; i < arrayToLoopThrough.length; i++) {
             counter += 1;
         }
 
@@ -132,7 +133,7 @@ public class Lecture {
         int counter = 0;
 
         //     Start;       Keep going while         Increment by one;
-        for (int i = 0; i <= arrayToLoopThrough.length; i++) {
+        for (int i = 0; i < arrayToLoopThrough.length; i++) {
             counter = counter + 1;
         }
 
@@ -149,7 +150,7 @@ public class Lecture {
         int sum = 0;
 
         //     Start;       Keep going while       Increment by;
-        for (int i = 0; i < arrayToLoopThrough.length; i = i + 1) {
+        for (int i = 0; i < arrayToLoopThrough.length; i = i + 2) {
             sum = sum + arrayToLoopThrough[i];
         }
 
