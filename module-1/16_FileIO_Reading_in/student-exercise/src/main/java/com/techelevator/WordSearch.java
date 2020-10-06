2fbd2595e360054ca3da24700050d77a86b57e2d
Shane Craig <shanecraig99@live.com>
2020-10-05 20:42:16 -0400

    Submitting Week 4 Day 1 exercise

--- module-1/16_FileIO_Reading_in/student-exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16_FileIO_Reading_in/student-exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,10 +1,111 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class WordSearch {
 
 	public static void main(String[] args) {
 	
+		System.out.println("What is the file that should be searched?");
+		
+		File inputFile = getInputFileFromUser();
+		
+		System.out.println("What is the search word you are looking for?");
+		
+		Scanner keyboard = new Scanner (System.in);
+		
+		String wordSearch = keyboard.nextLine();
+		
+		System.out.println("Should the search be case sensitive?" + " (Y/N)");
+		
+		String caseSensitive = keyboard.nextLine();
+		
+		int sentenceLine = 0;
+		
+		if (caseSensitive.equalsIgnoreCase("y")) {
+			
+			try(Scanner fileScanner = new Scanner(inputFile)){
+				
+				while(fileScanner.hasNextLine()) {
+					
+					sentenceLine++;
+					
+					String wordSentence = fileScanner.nextLine();
+					
+					if (wordSentence.contains(" " + wordSearch + " "))
+					
+					System.out.println("(" + sentenceLine + ")" + " " + wordSentence);
+					
+				}
+			}
+			
+			catch(InputMismatchException e) {
+				System.out.println("Wrong input! Please follow input directions!");
+			}
+			
+			catch(FileNotFoundException e) {
+				System.out.println("File was not found!!!");
+			}
+		}
+		
+		if(caseSensitive.equalsIgnoreCase("n")){
 
+			try(Scanner fileScanner = new Scanner(inputFile)) {
+			
+				while(fileScanner.hasNextLine()) {
+					
+					sentenceLine++;
+				
+					String wordSentence = fileScanner.nextLine();
+					
+					if (wordSentence.contains(" " + wordSearch.substring(0,1).toLowerCase() + " ") || wordSentence.contains(" " + wordSearch.substring(0,1).toUpperCase() + " ")) {
+					
+					System.out.println("(" + sentenceLine + ")" + " " + wordSentence);
+					
+					}
+				
+				}
+			
+			}
+		
+			catch(InputMismatchException e) {
+				System.out.println("Wrong input! Please follow input directions!");
+			}
+		
+			catch(FileNotFoundException e) {
+				System.out.println("File was not found!!!");
+			}
+		
+		}
+	}		
+	
+	private static File getInputFileFromUser() {
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.print("Please enter path to input file >>> ");
+		
+		String path = userInput.nextLine();
+		
+		File inputFile = new File(path);
+		
+		if(inputFile.exists() == false) { // checks for the existence of a file
+			
+			System.out.println(path+" does not exist");
+			
+			System.exit(1);
+			// Ends the program
+		} else if(inputFile.isFile() == false) {
+			
+			System.out.println(path+" is not a file");
+			
+			System.exit(1); // Ends the program
+		}
+		return inputFile;
 	}
+	
 
 }
