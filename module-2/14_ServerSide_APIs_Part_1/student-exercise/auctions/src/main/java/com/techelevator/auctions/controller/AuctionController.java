463b0413d55d90d02aebd24d393a2802e2f36be3
Shane Craig <shanecraig99@live.com>
2020-10-30 14:48:28 -0400

    Submitting Week 7 Day 4 Exercise

--- module-2/14_ServerSide_APIs_Part_1/student-exercise/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/14_ServerSide_APIs_Part_1/student-exercise/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -8,13 +8,75 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/auctions")
+@RequestMapping(path = "/auctions")
 public class AuctionController {
 
     private AuctionDAO dao;
-
+    
+    
     public AuctionController() {
         this.dao = new MemoryAuctionDAO();
     }
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Auction> searching(@RequestParam(value = "title_like", defaultValue = "")String title_like, @RequestParam (value = "currentBid_lte", defaultValue = "0") double currentBid_lte){
+    	
+    	if (title_like == " " && currentBid_lte == 0) {
+    		
+    		return dao.list();
+    	}
+    	else if (title_like != " " && currentBid_lte == 0) {
+    		
+    		return dao.searchByTitle(title_like);
+    	
+    	}
+    	
+    	else if (title_like == " " && currentBid_lte > 0) {
+    		
+    		return dao.searchByPrice(currentBid_lte);
+    	}
+    	
+    	else {
+    		return dao.searchByTitleAndPrice(title_like, currentBid_lte);
+    	}
+    }
 
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public Auction get(@PathVariable int id) {
+    	return dao.get(id);
+	}
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public Auction create(@RequestBody Auction auction) {
+    	return dao.create(auction);
+    }
+    
+//    @RequestMapping(path = "?",method = RequestMethod.GET)
+//    public List<Auction> searchByTitle({
+//    	
+//    	if (title_like != null) {
+//    		
+//    		return dao.searchByTitle(title_like);
+//    	}
+//    	
+//    	else {
+//    		
+//    		return dao.list();
+//    	}
+//    	
+//    }
+    
+//    @RequestMapping( method = RequestMethod.GET)
+//    public List <Auction> searchByPrice(@RequestParam(value = "currentBid", defaultValue = "0") double currentBid_lte){
+//		return dao.searchByPrice(currentBid_lte);
+//    	
+//    }
+//    
+//    @RequestMapping(method = RequestMethod.GET)
+//    public List <Auction> searchByTitleAndPrice (@RequestParam (value = "title", defaultValue = " ") String title_like, @RequestParam (value = "currentBid", defaultValue = "0") double currentBid_lte){
+//		return dao.searchByTitleAndPrice(title_like, currentBid_lte);
+//    	
+//    }
+    
+    
 }
