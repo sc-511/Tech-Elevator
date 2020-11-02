de3bce0c6a8fd87bdc52502c290ea438f659e56d
Shane Craig <shanecraig99@live.com>
2020-11-02 15:17:18 -0500

    Submitting Week 8 Day 1 Exercise

--- module-2/16_Authentication/student-exercise/server/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/16_Authentication/student-exercise/server/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -9,10 +9,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import java.lang.reflect.Array;
 import java.security.Principal;
 import java.util.List;
 
 @RestController
+@PreAuthorize("isAuthenticated()")
 @RequestMapping("/auctions")
 public class AuctionController {
 
@@ -21,7 +24,8 @@ public class AuctionController {
     public AuctionController() {
         this.dao = new MemoryAuctionDAO();
     }
-
+    
+    @PreAuthorize("permitAll")
     @RequestMapping( path = "", method = RequestMethod.GET)
     public List<Auction> list(@RequestParam(defaultValue = "") String title_like, @RequestParam(defaultValue = "0") double currentBid_lte) {
 
@@ -34,32 +38,37 @@ public class AuctionController {
 
         return dao.list();
     }
-
+    
+   
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public Auction get(@PathVariable int id) throws AuctionNotFoundException {
         return dao.get(id);
     }
-
+    
+    @PreAuthorize("hasAnyRole('CREATOR' , 'ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping( path = "", method = RequestMethod.POST)
     public Auction create(@Valid @RequestBody Auction auction) {
         return dao.create(auction);
     }
-
+    
+    @PreAuthorize("hasAnyRole('CREATOR' , 'ADMIN')")
     @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
     public Auction update(@Valid @RequestBody Auction auction, @PathVariable int id) throws AuctionNotFoundException {
         return dao.update(auction, id);
     }
-
+    
+    @PreAuthorize("hasAnyRole('CREATOR', 'ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable int id) throws AuctionNotFoundException {
         dao.delete(id);
     }
 
-    @RequestMapping( path = "/whoami")
-    public String whoAmI() {
-        return "";
+    @RequestMapping( path = "/whoami", method = RequestMethod.GET)
+    public String whoAmI(Principal P) {
+    	
+        return P.getName();
     }
 
 }
