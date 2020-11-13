f766d06810f9e6456ec1c4df4258ef58e7091761
Tom Medvitz <tom@techelevator.com>
2020-11-13 08:21:11 -0500

    TEnmo solutions

---
+++ module-2/20_TEnmo_Capstone/tenmo-server/src/test/java/com/techelevator/tenmo/controller/AuthenticationControllerTests.java
@@ -0,0 +1,7 @@
+package com.techelevator.tenmo.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(controllers = AuthenticationController.class)
+public class AuthenticationControllerTests {
+}
