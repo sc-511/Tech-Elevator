6b843bc3f488a104e57898873f82c79b288ec76d
Shane Craig <70282950+sc-511@users.noreply.github.com>
2020-11-17 23:23:38 -0500

    Add files via upload

---
+++ Capstones/java-capstone-module-2-team-9/tenmo-server/src/main/java/com/techelevator/tenmo/model/Authority.java
@@ -0,0 +1,43 @@
+package com.techelevator.tenmo.model;
+
+import java.util.Objects;
+
+import javax.validation.constraints.NotEmpty;
+
+public class Authority {
+
+	@NotEmpty (message = "The field `name` should not be blank.")
+   private String name;
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public Authority(String name) {
+      this.name = name;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Authority authority = (Authority) o;
+      return name == authority.name;
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(name);
+   }
+
+   @Override
+   public String toString() {
+      return "Authority{" +
+         "name=" + name +
+         '}';
+   }
+}
---
+++ Capstones/java-capstone-module-2-team-9/tenmo-server/src/main/java/com/techelevator/tenmo/model/LoginDTO.java
@@ -0,0 +1,38 @@
+package com.techelevator.tenmo.model;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * DTO for storing a user's credentials.
+ */
+public class LoginDTO {
+	
+	@NotEmpty (message = "The field `username` should not be blank.")
+   private String username;
+	@NotEmpty(message = "The field `password` should not be blank.")
+   private String password;
+
+   public String getUsername() {
+      return username;
+   }
+
+   public void setUsername(String username) {
+      this.username = username;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   @Override
+   public String toString() {
+      return "LoginDTO{" +
+              "username='" + username + '\'' +
+              ", password='" + password + '\'' +
+              '}';
+   }
+}
